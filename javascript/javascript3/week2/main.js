cc87d4b52a57a9cd7ed7c5ec0b442864a2121938
rajanibusani <brajanipharma@gmaill.com>
2020-12-23 12:49:45 +0100

    deleted variables which are storing moveelements

--- javascript/javascript3/week2/main.js
+++ javascript/javascript3/week2/main.js
@@ -1,24 +1,19 @@
 //One by one
-
-const redCirclePromise = moveElement(document.querySelector("ul.marks li:nth-child(1)"), { x: 20, y: 300 });
-const blueCirclePromise = moveElement(document.querySelector("ul.marks li:nth-child(2)"), { x: 400, y: 300 });
-const greenCirclePromise = moveElement(document.querySelector("ul.marks li:nth-child(3)"), { x: 400, y: 20 });
-
 function translateOneByOne() {
   const promise1 = new Promise(resolve => {
-    setTimeout(() => resolve(redCirclePromise), 1000)
+    setTimeout(() => resolve(moveElement(document.querySelector("ul.marks li:nth-child(1)"), { x: 20, y: 300 })), 1000)
   })
-    .then(() => {
+    .then(() => {      
       console.log("Red circle element has been moved");
     });
   const promise2 = new Promise(resolve => {
-    setTimeout(() => resolve(blueCirclePromise), 3000)
+    setTimeout(() => resolve(moveElement(document.querySelector("ul.marks li:nth-child(2)"), { x: 400, y: 300 })), 3000)
   })
     .then(() => {
       console.log("Blue circle element has been moved");
     });
   const promise3 = new Promise(resolve => {
-    setTimeout(() => resolve(greenCirclePromise), 5000)
+    setTimeout(() => resolve(moveElement(document.querySelector("ul.marks li:nth-child(3)"), { x: 400, y: 20 })), 5000)
   })
     .then(() => {
       console.log("Green circle element has been moved");
@@ -31,12 +26,15 @@ translateOneByOne();
 
 async function translateAllAtOnce() {
   try {
-    await Promise.all([redCirclePromise, blueCirclePromise, greenCirclePromise])
-    console.log("all color circles moved and reached at a time");
+  await Promise.all(
+    [moveElement(document.querySelector("ul.marks li:nth-child(1)"), { x: 20, y: 300 }), 
+     moveElement(document.querySelector("ul.marks li:nth-child(2)"), { x: 400, y: 300 }), 
+     moveElement(document.querySelector("ul.marks li:nth-child(3)"), { x: 400, y: 20 })])
+   console.log("all color circles moved and reached at a time");
   } catch (error) {
     console.log(error);
   }
 }
-translateAllAtOnce();
+//translateAllAtOnce();
 
 
